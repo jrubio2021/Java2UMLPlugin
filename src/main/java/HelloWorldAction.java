7367653387dd193e5de6b4cf8092473bf982da43
JoePachecoMedia <pacheco.man.jose@gmail.com>
2022-03-21 17:05:24 -0500

    Jose's first Commit

--- src/main/java/HelloWorldAction.java
+++ src/main/java/HelloWorldAction.java
@@ -1,6 +1,9 @@
 import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.actionSystem.AnActionEvent;
+import com.intellij.openapi.actionSystem.CommonDataKeys;
+import com.intellij.openapi.project.Project;
 import com.intellij.openapi.ui.Messages;
+import com.intellij.pom.Navigatable;
 import org.jetbrains.annotations.NotNull;
 
 public class HelloWorldAction extends AnAction {
@@ -8,5 +11,17 @@ public class HelloWorldAction extends AnAction {
     public void actionPerformed(@NotNull AnActionEvent e) {
         Messages.showInfoMessage("Hello World", "Info");
 
+        Project currentProject = e.getProject();
+        StringBuilder message =
+                new StringBuilder(e.getPresentation().getText() + "*****");
+        Navigatable selectedElement = e.getData(CommonDataKeys.NAVIGATABLE);
+        if (selectedElement != null) {
+            message.append("\nSelected****:").append(selectedElement);
+        }
+        String title = e.getPresentation().getDescription();
+        Messages.showMessageDialog(currentProject,
+                message.toString(),
+                title,
+                Messages.getInformationIcon());
     }
 }
