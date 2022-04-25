6d98ef34e3892201a826d29e5737cc999d58ea89
jrubio2021 <jrubi05@gmail.com>
2022-04-24 21:32:09 -0500

    PopDialog to Turn into UML

--- src/main/java/actions/Java2UMLAction.java
+++ src/main/java/actions/Java2UMLAction.java
@@ -1,16 +1,19 @@
 package actions;
 
-import com.intellij.openapi.actionSystem.ActionManager;
-import com.intellij.openapi.actionSystem.AnAction;
-import com.intellij.openapi.actionSystem.AnActionEvent;
-import com.intellij.openapi.actionSystem.IdeActions;
+import com.intellij.openapi.actionSystem.*;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.ui.Messages;
+import com.intellij.openapi.vcs.checkin.CheckinHandler;
 import com.intellij.openapi.wm.ToolWindow;
 import com.intellij.openapi.wm.ToolWindowManager;
+import com.intellij.pom.Navigatable;
+import com.thoughtworks.xstream.mapper.Mapper;
 import org.jetbrains.annotations.NotNull;
 
 import javax.swing.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 
 public class Java2UMLAction extends AnAction {
@@ -22,31 +25,42 @@ public class Java2UMLAction extends AnAction {
         Project currentProject = e.getProject();
         //ToolWindow window = ToolWindowManager.getInstance(currentProject).getToolWindow("window");
         //window.show();
-        AnAction copy = ActionManager.getInstance().getAction(IdeActions.ACTION_COPY);
-        copy.actionPerformed(e);
+        //AnAction copy = ActionManager.getInstance().getAction(IdeActions.ACTION_COPY);
+        //copy.actionPerformed(e);
 
         AnAction export = ActionManager.getInstance().getAction(IdeActions.ACTION_EXPORT_TO_TEXT_FILE);
-        export.actionPerformed(e);
+        //export.actionPerformed(e);
 
-        AnAction enterEditor = ActionManager.getInstance().getAction(IdeActions.ACTION_EDITOR_ENTER);
-        enterEditor.actionPerformed(e);
+        //AnAction enterEditor = ActionManager.getInstance().getAction(IdeActions.ACTION_EDITOR_ENTER);
+        //enterEditor.actionPerformed(e);
 
-        AnAction paste = ActionManager.getInstance().getAction(IdeActions.ACTION_PASTE);
-        paste.actionPerformed(e);
+        //AnAction paste = ActionManager.getInstance().getAction(IdeActions.ACTION_CALL_HIERARCHY);
+        //paste.actionPerformed(e);
 
         AnAction newWindow = ActionManager.getInstance().getAction(IdeActions.ACTION_FILE_STRUCTURE_POPUP);
-        newWindow.actionPerformed(e);
+        //newWindow.actionPerformed(e);
 
-        AnAction makeMod = ActionManager.getInstance().getAction(IdeActions.ACTION_MAKE_MODULE);
-        makeMod.actionPerformed(e);
+        //AnAction makeMod = ActionManager.getInstance().getAction(IdeActions.ACTION_MAKE_MODULE);
+        //makeMod.actionPerformed(e);
+
+        AnAction hierarchy = ActionManager.getInstance().getAction(IdeActions.ACTION_METHOD_HIERARCHY);
+        //hierarchy.actionPerformed(e);
 
 
 
 
-        String stringExport = export.getTemplateText();
-        export.displayTextInToolbar();
 
-        Messages.showYesNoCancelDialog(currentProject, stringExport, "Title",null);
+        //String stringExport = newWindow.getTemplateText();
+        //newWindow.displayTextInToolbar();
+
+        final int answer = Messages.showYesNoCancelDialog(currentProject, "Turn Code into UML?", "Welcome to JAVA2UML!", null);
+        if(answer == Messages.YES){
+            newWindow.actionPerformed(e);
+            //hierarchy.actionPerformed(e);
+            //export.actionPerformed(e);
+
+        }
+
        /* ScriptPython sp = new ScriptPython();
         String msg1 = sp.runScript();
         Messages.showInfoMessage("Info"); */
